51506b7aad715f56a81eb7ca15c83291d06decda
Evan Crook <evannc@gmail.com>
2020-01-30 13:59:15 -0600

    Load constants and print them

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -4,24 +4,29 @@ use crate::value::Value;
 #[derive(Debug)]
 pub struct Bytecode {
     pub code: Vec<u8>,
-    constants: Vec<Value>,
-    lines: Vec<i32>
+    pub constants: Vec<Value>,
+    lines: Vec<u32>,
 }
 
 impl Bytecode {
     pub fn new() -> Self {
-        Bytecode { code: Vec::new(), constants: Vec::new(), lines: Vec::new() }
+        Bytecode {
+            code: Vec::new(),
+            constants: Vec::new(),
+            lines: Vec::new(),
+        }
     }
 
-    pub fn add_code(&mut self, opcode: u8, line: i32) -> () {
+    pub fn add_code(&mut self, opcode: u8, line: u32) -> () {
         self.code.push(opcode);
         self.lines.push(line);
     }
 
-    pub fn add_constant(&mut self, val: Value) -> usize {
+    // truncates length to u8, as the LOAD_CONST operation works on a single byte
+    pub fn add_constant(&mut self, val: Value, line: u32) -> u8 {
         self.constants.push(val);
-        return self.constants.len() - 1;
-
+        self.lines.push(line);
+        return (self.constants.len() - 1) as u8;
     }
 }
 
@@ -29,6 +34,12 @@ impl Bytecode {
 fn test_bytecode() {
     let mut code = Bytecode::new();
 
+    // Equivalent to the following assembly:
+    // 0    LOAD_CONST      True
+    // 0    RETURN
+    let constant_idx = code.add_constant(Value::True, 0);
+    code.add_code(Op::LoadConstant as u8, 0);
+    code.add_code(constant_idx, 0);
     code.add_code(Op::Return as u8, 0);
 
     println!("{:#?}", code);
--- src/vm.rs
+++ src/vm.rs
@@ -16,6 +16,7 @@ pub struct VM {
 }
 
 impl VM {
+    // Takes ownership of a Bytecode.
     fn new(bytecode: Bytecode) -> Self {
         VM { bytecode, ip: 0}
     }
@@ -25,29 +26,45 @@ impl VM {
             let byte = self.read_byte();
             match Op::from_byte(byte) {
                 Some(Op::Return) => return Ok(Value::Null),
+
+                Some(Op::LoadConstant) => {
+                    let constant = self.read_constant();
+                    println!("{:?}", constant);
+                }
+
                 _ => return Err(VMError::InvalidOpcode(byte))
             }
         }
     }
 
     #[inline]
+    // Crafting Interpreters uses a real pointer into self.bytecode.code as ip
+    // which Rust will probably make hard for you, but may be faster?
     fn read_byte(&mut self) -> u8 {
         let byte = self.bytecode.code[self.ip];
         self.ip += 1;
         byte
+    }
+
+    #[inline]
+    fn read_constant(&mut self) -> &Value {
+        let idx = self.read_byte() as usize;
+        &self.bytecode.constants[idx]
     }
 }
 
 #[test]
 fn test_vm() {
     let mut code = Bytecode::new();
-    code.add_code(0u8, 0);
+    let constant_idx = code.add_constant(Value::True, 0);
+    code.add_code(Op::LoadConstant as u8, 0);
+    code.add_code(constant_idx, 0);
+    code.add_code(Op::Return as u8, 0);
 
     let mut vm = VM::new(code);
 
     let res = vm.run();
 
-    // todo: assert equals Ok(Value::Null)
     println!("{:#?}", res)
 }
 
